a764d2fb82d5aa81c26aaf8021945d23d3e917c8
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-14 14:29:53 -0500

    added function to fetch from API and console log to check

--- assets/js/results.js
+++ assets/js/results.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, options);
   });
+  
+let userQuery = location.search.split('=').pop();
+let apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=${userQuery}&fields=image_id,title,artist_display,gallery_title,on_loan_display,is_on_view`
+fetch(apiUrl)
+.then(function(response){
+    return response.json();
+})
+.then(function(results){
+    console.log(results.data);
+})
+console.log(apiUrl);
+
 //    var instance = M.Materialbox.getInstance(elem);
