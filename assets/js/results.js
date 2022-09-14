c1b04b009b9d6f818871d6d96b68d34fc9806ee0
Mgamboa <marianellag400@gmail.com>
2022-09-14 13:52:23 -0500

    results.html was added

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
   });
-=======
-// let userQ = location.search
->>>>>>> ae4e17781c646b00ed4e681e7631371933b7a384
+//    var instance = M.Materialbox.getInstance(elem);
