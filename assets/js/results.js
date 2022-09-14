fa71b48d56f7fce3849552d37399e397fa1c0003
Mgamboa <marianellag400@gmail.com>
2022-09-14 14:34:08 -0500

    updated files

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,5 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.materialboxed');
+//     var instances = M.Materialbox.init(elems, options);
+//   });
 //    var instance = M.Materialbox.getInstance(elem);
+// const imageRef = React.useRef();
+
+// React.useEffect(() => {
+//    M.Materialbox.init(imageRef.current);
+// }, [imageRef]);
+
+useEffect(() => {
+  var elems = document.querySelectorAll(".materialboxed");
+  M.Materialbox.init(elems);
+}, []);
