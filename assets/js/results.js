a0bc73a8bfd37f6b9afce338ee883aa5f07d73a6
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-09-15 13:33:56 -0500

    working on appending information to results.html

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,14 +1,24 @@
 let userQuery = location.search.split('=').pop();
 let apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=${userQuery}&fields=image_id,title,artist_display,gallery_title,on_loan_display,is_on_view`
-fetch(apiUrl)
+let artworkDisplay = $('#artwork-conteiner');
+let artistBio = $('#artist-info')
+
+function displayArtwork (){    
+    fetch(apiUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (results) {
         console.log(results.data);
+        for(let i = 0; i < results.data.length; i++){
+            let artist = $('<h4>').text(results.data[i].artist_display);
+            artistBio.append(artist);
+        }
     })
+}
 console.log(apiUrl);
 
+$(document).ready(displayArtwork)
 $(document).ready(function () {
     $('.materialboxed').materialbox();
 });
