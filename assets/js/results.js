2ee4de90839cbea2145b2f4cd2fbd424264e6048
Mgamboa <marianellag400@gmail.com>
2022-09-15 10:33:36 -0500

    results.html updated

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,14 +1,14 @@
 let userQuery = location.search.split('=').pop();
 let apiUrl = `https://api.artic.edu/api/v1/artworks/search?q=${userQuery}&fields=image_id,title,artist_display,gallery_title,on_loan_display,is_on_view`
 fetch(apiUrl)
-.then(function(response){
-    return response.json();
-})
-.then(function(results){
-    console.log(results.data);
-})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (results) {
+        console.log(results.data);
+    })
 console.log(apiUrl);
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-  });
+});
